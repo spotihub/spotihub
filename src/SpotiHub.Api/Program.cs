24af8e39e84b65644326b4a9c2a81ab39c5f994e
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-10-29 17:06:13 +0200

    feat: add basic changes to Program and Startup

--- src/SpotiHub.Api/Program.cs
+++ src/SpotiHub.Api/Program.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Incremental.Common.Configuration;
+using Incremental.Common.Logging;
+using Lamar.Microsoft.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace SpotiHub.Api
 {
@@ -13,11 +18,46 @@ namespace SpotiHub.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var assembly = Assembly.GetCallingAssembly().GetName().Name;
+            
+            try
+            {
+                var host = CreateHostBuilder(args).Build();
+                
+                if (Log.Logger is not null)
+                {
+                    Log.Information("Starting {Service}", assembly);
+                }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                if (Log.Logger is not null)
+                {
+                    Log.Fatal(ex, "{Service} terminated unexpectedly", assembly);
+                }
+            }
+            finally
+            {
+                if (Log.Logger is not null)
+                {
+                    Log.CloseAndFlush();
+                }
+            }
+        }
+        
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .UseLamar()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.AddCommonLogging();
+                })
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddCommonConfiguration();
+                });
     }
 }
--- src/SpotiHub.Api/Startup.cs
+++ src/SpotiHub.Api/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lamar;
+using Lamar.Diagnostics;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -14,8 +16,15 @@ namespace SpotiHub.Api
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(ServiceRegistry services)
         {
+            services.AddControllers();
+            
+            #region Healthcheks
+            
+            services.CheckLamarConfiguration();
+            
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,7 +39,7 @@ namespace SpotiHub.Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
