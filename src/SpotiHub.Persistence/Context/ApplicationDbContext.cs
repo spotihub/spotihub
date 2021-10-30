dac134195cf5151676139925d7efa9fb3422fbab
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-10-30 13:49:25 +0200

    feat: create ApplicationDbContext

---
+++ src/SpotiHub.Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SpotiHub.Core.Entity;
+
+namespace SpotiHub.Persistence.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
