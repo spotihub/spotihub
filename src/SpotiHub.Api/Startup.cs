49d499a997aed56c373bcd5082a187cd6420602c
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-10-31 18:32:23 +0100

    feat: add Identity dependencies in Startup

--- src/SpotiHub.Api/Startup.cs
+++ src/SpotiHub.Api/Startup.cs
@@ -4,21 +4,61 @@ using System.Linq;
 using System.Threading.Tasks;
 using Lamar;
 using Lamar.Diagnostics;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SpotiHub.Core.Entity;
+using SpotiHub.Persistence.Context;
 
 namespace SpotiHub.Api
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(ServiceRegistry services)
         {
             services.AddControllers();
+            services.AddHttpContextAccessor();
+            
+            #region Identity
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                var connectionString = Configuration["IDENTITY_CONNECTION_STRING"];
+                options.UseNpgsql(connectionString, optionsBuilder =>
+                {
+                    optionsBuilder
+                        .MigrationsAssembly("SpotiHub.Persistence.Migrations");
+                });
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = false;
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            }).AddEntityFrameworkStores<ApplicationDbContext>();
+
+            #endregion
+
+            #region Auth
+
+            #endregion
             
             #region Healthcheks
             
@@ -34,8 +74,15 @@ namespace SpotiHub.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()?.CreateScope();
+            serviceScope?.ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.Migrate();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
