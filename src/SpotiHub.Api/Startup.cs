fd1d691e6ca375e5861a46d6b7b81829a58371b4
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-11-25 19:08:26 +0100

    fix: correct Configure call in Startup

--- src/SpotiHub.Api/Startup.cs
+++ src/SpotiHub.Api/Startup.cs
@@ -29,7 +29,7 @@ namespace SpotiHub.Api
         
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(ServiceRegistry services)
+        public void ConfigureContainer(ServiceRegistry services)
         {
             services.AddControllers();
             services.AddHttpContextAccessor();
