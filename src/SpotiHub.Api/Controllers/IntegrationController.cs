119e1ea6fea486581b60e69bfdeccfaf47275b7d
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-05-09 17:07:41 +0200

    feat: implement GitHub integration in controller

--- src/SpotiHub.Api/Controllers/IntegrationController.cs
+++ src/SpotiHub.Api/Controllers/IntegrationController.cs
@@ -1,29 +1,70 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
+using Incremental.Common.Authentication.Jwt;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Octokit;
-using Octokit.GraphQL;
-using Octokit.GraphQL.Core;
-using Octokit.GraphQL.Internal;
-using Octokit.GraphQL.Model;
+using SpotiHub.Core.Application.Services;
+using SpotiHub.Core.Application.Services.ApplicationUser;
+using SpotiHub.Core.Application.Services.GitHub;
+using SpotiHub.Core.Entity;
 
-namespace SpotiHub.Api.Controllers
+namespace SpotiHub.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class IntegrationController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class IntegrationController : ControllerBase
+    private readonly IApplicationUserService _applicationUserService;
+    private readonly IGitHubAuthService _gitHubAuthService;
+    private readonly ITokenService _tokenService;
+    private readonly IConfiguration _configuration;
+
+    public IntegrationController(IApplicationUserService applicationUserService, IGitHubAuthService gitHubAuthService,
+        ITokenService tokenService, IConfiguration configuration)
     {
-        [HttpPost("github", Name = "LinkGitHub")]
-        public async Task<IActionResult> LinkGitHub(CancellationToken cancellationToken)
+        _applicationUserService = applicationUserService;
+        _gitHubAuthService = gitHubAuthService;
+        _tokenService = tokenService;
+        _configuration = configuration;
+    }
+
+    [HttpGet("github", Name = nameof(GitHub))]
+    public async Task<IActionResult> GitHub(CancellationToken cancellationToken)
+    {
+        var destination = await _gitHubAuthService.GetLoginUrl(cancellationToken);
+
+        if (cancellationToken.IsCancellationRequested)
         {
-            throw new NotImplementedException();
+            return Problem();
         }
-        
-        [HttpPost("spotify", Name = "LinkSpotify")]
-        public async Task<IActionResult> LinkSpotify(CancellationToken cancellationToken)
+
+        return Redirect(destination);
+    }
+
+    [HttpGet("github/authorize", Name = nameof(GitHubAuthorize))]
+    public async Task<ActionResult> GitHubAuthorize([FromQuery] string code, CancellationToken cancellationToken)
+    {
+        var token = await _gitHubAuthService.Authorize(code, cancellationToken);
+
+        if (!token.HasValue)
         {
-            throw new NotImplementedException();
+            return Problem();
         }
+
+        var user = await _applicationUserService.GetOrCreate(token.Value.Token, token.Value.Scopes, cancellationToken);
+
+        if (user is null)
+        {
+            return Problem();
+        }
+
+        var tokenInfo = await _tokenService.GenerateTokenAsync(user.Id);
+
+        return Redirect($"{_configuration["SPA_BASE_URI"]}/login?token={tokenInfo.Token}&refreshToken={tokenInfo.RefreshToken}");
+    }
+
+    [HttpPost("spotify", Name = nameof(LinkSpotify))]
+    public async Task<IActionResult> LinkSpotify(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
     }
 }
