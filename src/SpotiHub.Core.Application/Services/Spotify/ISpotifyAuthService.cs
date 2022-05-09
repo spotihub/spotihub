d39f85b89acc3bc4dd36827244feab5c75131aed
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-05-09 22:59:37 +0200

    feat: add Authorize method to ISpotifyAuthService

--- src/SpotiHub.Core.Application/Services/Spotify/ISpotifyAuthService.cs
+++ src/SpotiHub.Core.Application/Services/Spotify/ISpotifyAuthService.cs
@@ -2,5 +2,6 @@ namespace SpotiHub.Core.Application.Services.Spotify;
 
 public interface ISpotifyAuthService
 {
-    Task<string> GetLoginUrl(CancellationToken cancellationToken = default);
+    Task<string> GetLoginUrl(string user, CancellationToken cancellationToken = default);
+    Task Authorize(string code, string state, CancellationToken cancellationToken = default);
 }
