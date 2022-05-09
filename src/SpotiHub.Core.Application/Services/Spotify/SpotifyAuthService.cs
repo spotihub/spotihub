894b99798f39c8e06b8a55f2d48d8aa0d7b19d9d
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-05-09 22:59:57 +0200

    feat: implement Authorize for SpotifyAuthService

--- src/SpotiHub.Core.Application/Services/Spotify/SpotifyAuthService.cs
+++ src/SpotiHub.Core.Application/Services/Spotify/SpotifyAuthService.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SpotifyAPI.Web;
@@ -10,16 +12,25 @@ public class SpotifyAuthService : ISpotifyAuthService
     private readonly ILogger<SpotifyAuthService> _logger;
     private readonly ISpotifyClient _spotifyClient;
     private readonly SpotifyOptions _options;
+    private readonly SpotifyClientConfig _spotifyClientConfig;
+    private readonly UserManager<Entity.ApplicationUser> _userManager;
 
-    public SpotifyAuthService(ILogger<SpotifyAuthService> logger, ISpotifyClient spotifyClient, IOptions<SpotifyOptions> options)
+    public SpotifyAuthService(ILogger<SpotifyAuthService> logger, ISpotifyClient spotifyClient, IOptions<SpotifyOptions> options,
+        SpotifyClientConfig spotifyClientConfig, UserManager<Entity.ApplicationUser> userManager)
     {
         _logger = logger;
         _spotifyClient = spotifyClient;
+        _spotifyClientConfig = spotifyClientConfig;
+        _userManager = userManager;
         _options = options.Value;
     }
 
-    public Task<string> GetLoginUrl(CancellationToken cancellationToken = default)
+    public async Task<string> GetLoginUrl(string user, CancellationToken cancellationToken = default)
     {
+        var state = Guid.NewGuid().ToString();
+
+        await _userManager.AddLoginAsync(await _userManager.FindByIdAsync(user), new UserLoginInfo("spotify", state, "Transition"));
+        
         var loginRequest = new LoginRequest(_options.RedirectUrl, _options.ClientId, LoginRequest.ResponseType.Code)
         {
             Scope = new[]
@@ -27,9 +38,35 @@ public class SpotifyAuthService : ISpotifyAuthService
                 Scopes.UserReadPrivate,
                 Scopes.UserReadRecentlyPlayed,
                 Scopes.UserReadRecentlyPlayed
-            }
+            },
+            State = state
         };
 
-        return Task.FromResult(loginRequest.ToUri().ToString());
+        return loginRequest.ToUri().ToString();
+    }
+
+    public async Task Authorize(string code, string state,
+        CancellationToken cancellationToken = default)
+    {
+        var user = await _userManager.FindByLoginAsync("spotify", state);
+
+        if (user is null)
+        {
+            return;
+        }
+        
+        var response = await new OAuthClient(_spotifyClientConfig)
+            .RequestToken(new AuthorizationCodeTokenRequest(
+                _options.ClientId,
+                _options.ClientSecret,
+                code,
+                _options.RedirectUrl));
+
+        await _userManager.RemoveLoginAsync(user, "spotify", state);
+
+        await _userManager.AddLoginAsync(user, new UserLoginInfo("spotify:token", response.AccessToken, "spotify:token"));
+        await _userManager.AddLoginAsync(user, new UserLoginInfo("spotify:refresh_token", response.RefreshToken, "spotify:refresh_token"));
+
+        await _userManager.AddClaimsAsync(user, response.Scope.Split(',').Select(scope => new Claim("spotify:scope", scope)));
     }
 }
