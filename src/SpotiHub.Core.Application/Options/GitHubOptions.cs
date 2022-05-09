c233b033fee92da95747d595dc9abdcee730e921
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-05-09 17:00:56 +0200

    feat: add GitHubOptions model

---
+++ src/SpotiHub.Core.Application/Options/GitHubOptions.cs
@@ -0,0 +1,8 @@
+namespace SpotiHub.Core.Application.Options;
+
+public class GitHubOptions
+{
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+    public string Name { get; set; }
+}
