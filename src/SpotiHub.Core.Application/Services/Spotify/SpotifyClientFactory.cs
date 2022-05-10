fcb961ac2157be94715daa1660aa9284d58d0711
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-05-10 09:59:49 +0200

    feat: improve SpotifyClientFactory with cached auths

--- src/SpotiHub.Core.Application/Services/Spotify/SpotifyClientFactory.cs
+++ src/SpotiHub.Core.Application/Services/Spotify/SpotifyClientFactory.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SpotifyAPI.Web;
@@ -12,13 +14,15 @@ public class SpotifyClientFactory : ISpotifyClientFactory
     private readonly UserManager<Entity.ApplicationUser> _userManager;
     private readonly SpotifyClientConfig _defaultClientConfig;
     private readonly SpotifyOptions _options;
+    private readonly IDistributedCache _distributedCache;
 
     public SpotifyClientFactory(ILogger<SpotifyClientFactory> logger, UserManager<Entity.ApplicationUser> userManager, 
-        SpotifyClientConfig defaultClientConfig, IOptions<SpotifyOptions> options)
+        SpotifyClientConfig defaultClientConfig, IOptions<SpotifyOptions> options, IDistributedCache distributedCache)
     {
         _logger = logger;
         _userManager = userManager;
         _defaultClientConfig = defaultClientConfig;
+        _distributedCache = distributedCache;
         _options = options.Value;
     }
 
@@ -29,11 +33,21 @@ public class SpotifyClientFactory : ISpotifyClientFactory
 
     public async Task<ISpotifyClient> GetClientAsync(string userId, CancellationToken cancellationToken = default)
     {
-        var user = await _userManager.FindByIdAsync(userId);
-        
-        var authClient = await GetAuthClientAsync(cancellationToken);
+        var cachedToken = await _distributedCache.GetStringAsync(userId, cancellationToken);
+        var token = JsonSerializer.Deserialize<AuthorizationCodeRefreshResponse>(cachedToken);
 
-        var token = await authClient.RequestToken(await BuildRefreshRequest(user));
+        if (token is null)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            var authClient = await GetAuthClientAsync(cancellationToken);
+
+            token = await authClient.RequestToken(await BuildRefreshRequest(user));
+
+            await _distributedCache.SetStringAsync(userId, JsonSerializer.Serialize(token), token: cancellationToken);
+
+            await _userManager.AddLoginAsync(user, new UserLoginInfo("spotify:refresh_token", token.RefreshToken, "spotify:refresh_token"));
+        }
 
         return new SpotifyClient(_defaultClientConfig.WithToken(token.AccessToken));
     }
@@ -43,6 +57,8 @@ public class SpotifyClientFactory : ISpotifyClientFactory
         var logins = await _userManager.GetLoginsAsync(user);
 
         var refreshToken = logins.First(login => login.LoginProvider == "spotify:refresh_token").ProviderKey;
+
+        await _userManager.RemoveLoginAsync(user, "spotify:refresh_token", refreshToken);
 
         return new AuthorizationCodeRefreshRequest(_options.ClientId, _options.ClientSecret, refreshToken);
     }
