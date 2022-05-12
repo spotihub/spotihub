218583d21877bef850a52bf3e0b0331e3a4b246e
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-05-12 23:44:47 +0200

    feat: update options

--- src/SpotiHub.Core.Entity/Options.cs
+++ src/SpotiHub.Core.Entity/Options.cs
@@ -2,11 +2,17 @@ namespace SpotiHub.Core.Entity;
 
 public class Options
 {
+    public Options()
+    {
+        Enabled = true;
+        DefaultEmoji = "🎶";
+    }
+
     public bool Enabled { get; set; }
+    public string DefaultEmoji { get; set; }
     public bool LimitedAvailability { get; set; }
-    public bool DefaultEmoji { get; set; }
     public bool GenreEmojis { get; set; }
     public string? FallbackMessage { get; set; }
     public string? FallbackEmoji { get; set; }
-    public TimeSpan ClearAfter { get; set; }
+    public TimeSpan? ClearAfter { get; set; }
 }
