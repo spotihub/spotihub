5c5fa5164a8b115392f46078b31b90ea4bb04da6
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-05-12 23:50:20 +0200

    feat: update migrations

---
+++ src/SpotiHub.Persistence.Migrations/ApplicationDb/20220512214835_AddOptions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpotiHub.Persistence.Migrations.ApplicationDb
+{
+    public partial class AddOptions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Options",
+                columns: table => new
+                {
+                    ApplicationUserId = table.Column<string>(type: "text", nullable: false),
+                    Enabled = table.Column<bool>(type: "boolean", nullable: false),
+                    DefaultEmoji = table.Column<string>(type: "text", nullable: false),
+                    LimitedAvailability = table.Column<bool>(type: "boolean", nullable: false),
+                    GenreEmojis = table.Column<bool>(type: "boolean", nullable: false),
+                    FallbackMessage = table.Column<string>(type: "text", nullable: true),
+                    FallbackEmoji = table.Column<string>(type: "text", nullable: true),
+                    ClearAfter = table.Column<TimeSpan>(type: "interval", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Options", x => x.ApplicationUserId);
+                    table.ForeignKey(
+                        name: "FK_Options_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Options");
+        }
+    }
+}
