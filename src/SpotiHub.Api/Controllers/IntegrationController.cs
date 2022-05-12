cb7a8a2fa4cec94b64ca042715bb31404e11ee00
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-05-12 10:07:08 +0200

    feat: move github signin to authentication controller and implement refresh

--- src/SpotiHub.Api/Controllers/IntegrationController.cs
+++ src/SpotiHub.Api/Controllers/IntegrationController.cs
@@ -2,8 +2,6 @@ using Incremental.Common.Authentication;
 using Incremental.Common.Authentication.Jwt;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using SpotiHub.Core.Application.Services.ApplicationUser;
-using SpotiHub.Core.Application.Services.GitHub;
 using SpotiHub.Core.Application.Services.Spotify;
 
 namespace SpotiHub.Api.Controllers;
@@ -12,54 +10,15 @@ namespace SpotiHub.Api.Controllers;
 [Route("api/[controller]")]
 public class IntegrationController : ControllerBase
 {
-    private readonly IApplicationUserService _applicationUserService;
     private readonly ISpotifyAuthService _spotifyAuthService;
-    private readonly IGitHubAuthService _gitHubAuthService;
     private readonly ITokenService _tokenService;
     private readonly IConfiguration _configuration;
 
-    public IntegrationController(IApplicationUserService applicationUserService, ISpotifyAuthService spotifyAuthService, IGitHubAuthService gitHubAuthService, ITokenService tokenService, IConfiguration configuration)
+    public IntegrationController(ISpotifyAuthService spotifyAuthService, ITokenService tokenService, IConfiguration configuration)
     {
-        _applicationUserService = applicationUserService;
         _spotifyAuthService = spotifyAuthService;
-        _gitHubAuthService = gitHubAuthService;
         _tokenService = tokenService;
         _configuration = configuration;
-    }
-
-    [HttpGet("github", Name = nameof(GitHub))]
-    public async Task<IActionResult> GitHub(CancellationToken cancellationToken)
-    {
-        var destination = await _gitHubAuthService.GetLoginUrl(cancellationToken);
-
-        if (cancellationToken.IsCancellationRequested)
-        {
-            return Problem();
-        }
-
-        return Redirect(destination);
-    }
-
-    [HttpGet("github/authorize", Name = nameof(GitHubAuthorize))]
-    public async Task<ActionResult> GitHubAuthorize([FromQuery] string code, CancellationToken cancellationToken)
-    {
-        var token = await _gitHubAuthService.Authorize(code, cancellationToken);
-
-        if (!token.HasValue)
-        {
-            return Problem();
-        }
-
-        var user = await _applicationUserService.GetOrCreate(token.Value.Token, token.Value.Scopes, cancellationToken);
-
-        if (user is null)
-        {
-            return Problem();
-        }
-
-        var tokenInfo = await _tokenService.GenerateTokenAsync(user.Id);
-
-        return Redirect($"{_configuration["SPA_BASE_URI"]}/login?token={tokenInfo.Token}&refreshToken={tokenInfo.RefreshToken}");
     }
 
     [Authorize]
@@ -79,7 +38,7 @@ public class IntegrationController : ControllerBase
     [HttpGet("spotify/authorize", Name = nameof(SpotifyAuthorize))]
     public async Task<IActionResult> SpotifyAuthorize([FromQuery] string code, [FromQuery] string state, CancellationToken cancellationToken)
     {
-        await _spotifyAuthService.Authorize(code, state, cancellationToken);
+        await _spotifyAuthService.AuthorizeAsync(code, state, cancellationToken);
         
         return Redirect($"{_configuration["SPA_BASE_URI"]}");
     }
