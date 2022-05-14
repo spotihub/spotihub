4444b8eb0618a70e88e5d9fe6e549c83db29d551
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-05-14 11:35:25 +0200

    feat: update Options entity

--- src/SpotiHub.Core.Entity/Options.cs
+++ src/SpotiHub.Core.Entity/Options.cs
@@ -2,17 +2,10 @@ namespace SpotiHub.Core.Entity;
 
 public class Options
 {
-    public Options()
-    {
-        Enabled = true;
-        DefaultEmoji = "🎶";
-    }
-
     public bool Enabled { get; set; }
-    public string DefaultEmoji { get; set; }
+    public Template Template { get; set; } = default!;
     public bool LimitedAvailability { get; set; }
     public bool GenreEmojis { get; set; }
-    public string? FallbackMessage { get; set; }
-    public string? FallbackEmoji { get; set; }
+    public Template? FallbackTemplate { get; set; }
     public TimeSpan? ClearAfter { get; set; }
 }
